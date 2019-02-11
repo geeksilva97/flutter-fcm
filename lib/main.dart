4bfd1d0a8841f823d6106cd8eba31e11d0cf679d
Edigleysson Silva <edigleyssonsilva@gmail.com>
2019-02-11 08:12:07 -0300

    Remote Config funcionando porém sem  RealTime

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'record.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
+
+
+Future<RemoteConfig> setupRemoteConfig() async {
+  final RemoteConfig remoteConfig = await RemoteConfig.instance;
+  remoteConfig.setConfigSettings(RemoteConfigSettings(debugMode: true));
+  remoteConfig.setDefaults(<String, dynamic> {
+    'app_title': 'Baby'
+  });
+
+  return remoteConfig;
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,11 +24,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FireFlutter',
       home: MyHomePage(),
+      // home: FutureBuilder<RemoteConfig>(
+      //   future: setupRemoteConfig(),
+      //   builder: (BuildContext context, AsyncSnapshot<RemoteConfig> snapshot) {
+      //     return snapshot.hasData ? MyHomePage(remoteConfig: snapshot.data) : Container(
+      //       child: Text('Remote Config has no data'),
+      //     );
+      //   },
+      // )
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -23,10 +45,64 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>{
 
   final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+  String appTitle = 'Baby Names';
+
+
+  void _showMessageDialog(String message) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Mensagem FCM'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('$message')
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Close'),
+              onPressed: (){
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
 
   @override
   void initState() {
     super.initState();
+
+    setupRemoteConfig().then((remoteConfig) {
+    print('RemoteConfig test');
+    print(remoteConfig.getString('app_title'));
+      // Using default duration to force fetching from remote server.
+      remoteConfig.fetch(expiration: const Duration(seconds: 0));
+      remoteConfig.activateFetched();
+      setState(() {
+        appTitle = remoteConfig.getString('app_title');
+      });
+    });
+
+    _firebaseMessaging.configure(
+    	onMessage: (Map<String, dynamic> message) async {
+    		print('onMessage: $message');
+    	},
+
+    	onLaunch: (Map<String, dynamic> message) {
+    		print('onLaunch: $message');
+    	},
+
+    	onResume: (Map<String, dynamic> message) {
+    		print('onResume: $message');
+    	}
+    );
 
     _firebaseMessaging.getToken().then((token) {
       print(token);
@@ -40,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Baby Names Votes'),
+        title: Text(appTitle),
       ),
       body: _buildBody(context)
     );
